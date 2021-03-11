533aa64a71cb70b1ea54f32fe3ba8bf18d43bf04
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-03-11 11:02:13 +0600

    react-router

--- src/components/TeamDetail/TeamDetail.js
+++ src/components/TeamDetail/TeamDetail.js
@@ -3,13 +3,16 @@ import { useParams } from 'react-router';
 import './TeamDetail.css';
 import male from '../../images/male.png'
 import female from '../../images/female.png'
+import twitterIcon from '../../images/Twitter.png'
+import youtubeIcon from '../../images/YouTube.png'
+import facebookIcon from '../../images/Facebook.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faFlagCheckered, faFutbol, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const TeamDetail = () => {
     const { teamId } = useParams()
     const [teamDetails, setTeamDetails] = useState([])
-    const { strTeam, intFormedYear, strCountry, strSport, strGender, strDescriptionEN } = teamDetails
+    const { strTeam, intFormedYear, strCountry, strSport, strGender, strTeamBadge, strTeamBanner, strDescriptionEN, strTwitter, strWebsite, strYoutube } = teamDetails
     // console.log(teamDetails);
 
     useEffect(() => {
@@ -17,8 +20,8 @@ const TeamDetail = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => setTeamDetails(data.teams[0]))
-    }, [teamId])
-    let ShowImage = male
+    }, [])
+    let ShowImage
     if (strGender === 'Male') {
         ShowImage = male
     }
@@ -27,6 +30,13 @@ const TeamDetail = () => {
     }
     return (
         <div className="team-details container" >
+            <div className="team-logo row">
+                <div className="col">
+                    {
+                        <img src={strTeamBadge} alt="" />
+                    }
+                </div>
+            </div>
             <div className="team-card row">
                 <div className="team-info col-md-6">
                     <h2>{strTeam}</h2>
@@ -43,8 +53,14 @@ const TeamDetail = () => {
             </div>
 
             <div className="team-description">
+                <img className="team-desc-img" src={strTeamBanner} />
                 <p><strong>Club Description :</strong></p>
                 <p>{strDescriptionEN}</p>
+                <div className="icon-list">
+                    <span><a href={'https://' + strTwitter}><img src={twitterIcon} /></a></span>
+                    <span><a href={'https://' + strYoutube}><img src={youtubeIcon} /></a></span>
+                    <span><a href={'https://' + strWebsite}><img src={facebookIcon} /></a></span>
+                </div>
             </div>
         </div>
 
